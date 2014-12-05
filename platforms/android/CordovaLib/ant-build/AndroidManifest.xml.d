cc4e471fbb5539c7ebe397b982379de2fddc9137
emcniece <emcniece@gmail.com>
2014-12-04 18:56:08 -0800

    removing platform build

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-G:\Programming\NodeJS\ionic-camera-test\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-G:\Programming\NodeJS\ionic-camera-test\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-G:\Programming\NodeJS\ionic-camera-test\platforms\android\ant-build\AndroidManifest.xml : \
-G:\Programming\NodeJS\ionic-camera-test\platforms\android\AndroidManifest.xml \
